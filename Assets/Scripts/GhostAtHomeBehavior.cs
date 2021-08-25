788b03b62066ff27d15648088299914b9e64ac9f
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-08-25 22:04:10 +0200

    Ghosts home behavior implemented

--- Assets/Scripts/GhostAtHomeBehavior.cs
+++ Assets/Scripts/GhostAtHomeBehavior.cs
@@ -1,18 +1,65 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-public class GhostAtHomeBehavior : MonoBehaviour
+using System.Collections;
+public class GhostAtHomeBehavior : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform inside;
+
+    public Transform outside;
+
+    private void OnDisable()
     {
-        
+        if (this.gameObject.activeSelf)
+        {
+            StartCoroutine(ExitTransition());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        StopAllCoroutines();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    if(this.enabled && collision.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            ghost.movement.SetDirection(-ghost.movement.direction);
+        }
+    }
+
+    
+    private IEnumerator ExitTransition()
+    {
+        ghost.movement.SetDirection(Vector2.up, true);
+        ghost.movement.rigidb.isKinematic = true;
+        ghost.movement.enabled = false;
+
+        Vector3 position = this.transform.position;
+        float duration = 0.5f;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(position, inside.position, elapsed / duration);
+            newPosition.z = position.z;
+            ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            Vector3 newPosition = Vector3.Lerp(inside.position, outside.position, elapsed / duration);
+            newPosition.z = position.z;
+            ghost.transform.position = newPosition;
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        ghost.movement.SetDirection(new Vector2(Random.value < 0.5f ? -1.0f : 1.0f, 0.0f), true);
+        ghost.movement.rigidb.isKinematic = false;
+        ghost.movement.enabled = true;
     }
 }
