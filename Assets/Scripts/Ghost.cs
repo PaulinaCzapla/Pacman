ef3c5ba97eca633f5e70c72067df2296bfc75898
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-08-25 22:05:03 +0200

    Ghost implemented

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -5,5 +5,63 @@ using UnityEngine;
 public class Ghost : MonoBehaviour
 {
     public int points;
-    
+
+    public Movement movement { get; private set; }
+    public GhostAtHomeBehavior home { get; private set; }
+    public ScaredGhostBehavior scared { get; private set; }
+    public ChasingGhostBehavior chase { get; private set; }
+    public ScatteredGhostBehavior scatter { get; private set; }
+
+    public GhostBehavior initialBehavior;
+
+    public Transform target;
+    private void Awake()
+    {
+        points = 200;
+        movement = GetComponent<Movement>();
+        home = GetComponent<GhostAtHomeBehavior>();
+        scared = GetComponent<ScaredGhostBehavior>();
+        scatter = GetComponent<ScatteredGhostBehavior>();
+        chase = GetComponent<ChasingGhostBehavior>();
+    }
+
+    private void Start()
+    {
+        ResetState();
+    }
+
+    public void ResetState()
+    {
+        this.gameObject.SetActive(true);
+        movement.ResetState();
+
+        scared.Disable();
+        chase.Disable();
+        scatter.Enable();
+
+        if(home!= initialBehavior)
+        {
+            home.Disable();
+        }
+
+        if(initialBehavior != null)
+        {
+            initialBehavior.Enable();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (this.scared.enabled)
+            {
+                FindObjectOfType<GameManager>().GhostEaten(this);
+            }
+            else
+            {
+                FindObjectOfType<GameManager>().PacmanEaten();
+            }
+        }
+    }
 }
