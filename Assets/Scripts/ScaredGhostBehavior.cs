abc7a9e4d5b8c4a635db9c123e5812da6e35f086
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-08-26 18:48:58 +0200

    implemented scared ghost behavior

--- Assets/Scripts/ScaredGhostBehavior.cs
+++ Assets/Scripts/ScaredGhostBehavior.cs
@@ -1,18 +1,108 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class ScaredGhostBehavior : MonoBehaviour
+public class ScaredGhostBehavior : GhostBehavior
 {
-    // Start is called before the first frame update
-    void Start()
+    public SpriteRenderer body;
+
+    public SpriteRenderer eyes;
+
+    public SpriteRenderer blue;
+
+    public SpriteRenderer white;
+
+    public bool eaten { get; private set; }
+
+    public override void Enable(float duration)
     {
-        
+        base.Enable(duration);
+
+        body.enabled = false;
+        eyes.enabled = false;
+        blue.enabled = true;
+        white.enabled = false;
+
+        Invoke(nameof(Flash), duration / 2.0f);
+    }
+    public override void Disable()
+    {
+        base.Disable();
+
+        body.enabled = true;
+        eyes.enabled = true;
+        blue.enabled = false;
+        white.enabled = false;
+    }
+    private void Flash()
+    {
+        if (!eaten)
+        {
+            blue.enabled = false;
+            white.enabled = true;
+            white.GetComponent<AnimatedSprite>().Restart();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Eaten()
     {
-        
+        eaten = true;
+        Vector3 position = this.ghost.home.inside.position;
+        position.z = this.ghost.transform.position.z;
+        this.ghost.transform.position = position; // TODO: ghosts going home, not just appearing inside
+
+        this.ghost.home.Enable(duration);
+
+        body.enabled = false;
+        eyes.enabled = true;
+        blue.enabled = false;
+        white.enabled = false;
     }
+
+    private void OnEnable()
+    {
+        this.ghost.movement.speedMultiplayer = 0.5f;
+        eaten = false;
+    }
+
+    private void OnDisable()
+    {
+        this.ghost.movement.speedMultiplayer = 1.0f;
+        eaten = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            if (this.enabled)
+            {
+                Eaten();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Node node = collision.GetComponent<Node>();
+
+        if (node != null && this.enabled)
+        {
+            Vector2 direction = Vector2.zero;
+            float maxDistance = float.MinValue;
+
+            foreach (Vector2 availableDirection in node.availableDirections)
+            {
+                Vector3 newPosition = this.transform.position + new Vector3(availableDirection.x, availableDirection.y, 0.0f);
+                float distance = (this.ghost.target.position - newPosition).sqrMagnitude;
+
+                if (distance > maxDistance)
+                {
+                    direction = availableDirection;
+                    maxDistance = distance;
+                }
+            }
+
+            this.ghost.movement.SetDirection(direction);
+        }
+    }
+
 }
